7844ec832579b721405b6742c10071baf36bfa4d
Mehmet Fatih YEŞİLAY <m.fatihyesilay@gmail.com>
2026-01-23 16:20:03 +0300

    proje temeli oluşturuldu

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'baglan.php';
+?>
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Kitap Sosyal Ağı</title>
+</head>
+<body>
+    <h1>Kitap Projesi Başladı</h1>
+    <p>Veritabanı bağlantısı durumu: <?php echo "Bağlantı Başarılı!"; ?></p>
+</body>
+</html>
